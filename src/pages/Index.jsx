7a597847b0658c16a0df43ac55be0f96a988f969
ashfaq <khanashfaq9423@gmail.com>
2025-10-21 23:50:46 +0530

    GUVI Assignment 6 smart expense tracker completed

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,6 +1,6 @@
 import Dashboard from "../components/Dashboard";
-import { ExpenseProvider } from "../context/ExpenseContext";
 import DashboardLayout from "../layouts/DashboardLayout";
+import { ExpenseProvider } from "./../context/ExpenseContext";
 
 const Index = () => {
   return (
